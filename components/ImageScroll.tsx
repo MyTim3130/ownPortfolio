444b66ca54e480ca917959432bc9229015ed0378
MyTim3130 <tin.hauslkramper@gmail.com>
2024-02-03 16:49:19 +0100

    fix

---
+++ components/ImageScroll.tsx
@@ -0,0 +1,118 @@
+'use client'
+import React, { useState, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ImageScroll = () => {
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const checkWidth = () => setIsWideScreen(window.innerWidth > 640);
+    checkWidth(); // Initial check
+
+    // Register resize event listener to handle window resizing
+    window.addEventListener('resize', checkWidth);
+
+    // GSAP animations setup
+    if (isWideScreen) {
+      gsap.fromTo("#photosLeft", 
+        { y: 1000 },
+        {
+          y: -3000,
+          scrollTrigger: {
+            trigger: "#PhotographyTab",
+            start: "top top",
+            end: "+=3000",
+            scrub: true,
+            markers: true
+          }
+        }
+      );
+
+      gsap.fromTo("#photosRight", 
+        { y: -6000 },
+        {
+          y: 3000,
+          scrollTrigger: {
+            trigger: "#PhotographyTab",
+            start: "top top",
+            end: "+=3000",
+            scrub: true,
+            markers: true
+          }
+        }
+      );
+    }
+
+    // Cleanup function to remove the resize event listener and kill ScrollTriggers when component unmounts
+    return () => {
+      window.removeEventListener('resize', checkWidth);
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, [isWideScreen]);
+
+  return (
+    <section className="mr-10 flex photos flex-col sm:flex-row -z-50" id="photosLeft">
+      <div className="h-fit sm:w-3/6 w-screen flex flex-col items-center">
+      <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw] bg-[url(/images/left1.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52  sm:h-[50vh] sm:w-[20vw] bg-[url(/images/left2.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left3.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left4.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left5.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left6.JPG)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left7.png)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/left8.JPG)] bg-cover rounded-2xl mb-7"></div>
+        </div>
+      </div>
+      
+      {isWideScreen && (
+        <div className="h-fit photos sm:w-3/6 w-screen flex flex-col items-center" id="photosRight">
+          <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right1.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right2.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right3.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right4.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right5.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right6.jpg)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right7.jpg)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+        <div>
+          <div className="h-52 w-52 sm:h-[50vh] sm:w-[20vw]  bg-[url(/images/right8.JPG)] bg-cover rounded-2xl mb-7"></div>{" "}
+        </div>
+
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default ImageScroll;
---
+++ components/LandingPage.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+const LandingPage = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <>
+      <div
+        className={`absolute w-screen h-screen justify-center items-center flex  ${
+          isHovered ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-300`}
+        id="hoverEffect"
+        style={{ zIndex: 10 }} // Keep z-index constant to prevent stacking context issues
+      >
+        <Image
+          className="h-96 sm:h-[50vh] w-auto"
+          src="/images/heroFront.png"
+          height={1000}
+          width={1000}
+          alt="missingImage"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+      </div>
+      <div className="absolute w-screen h-screen justify-center items-center flex -z-20 grayscale">
+        <Image
+          className="h-96 sm:h-[50vh] w-auto"
+          src="/images/heroBack.png"
+          height={1000}
+          width={1000}
+          alt="missingImage"
+        />
+      </div>
+      <section className="h-[95vh] w-screen flex justify-center items-center flex-col z-0">
+        <div
+          className="font-bold text-center flex flex-col justify-evenly items-center text-6xl sm:text-8xl h-3/6 w-[70vw] sm:text-[#ebebeb] text-sky-500 sm:mix-blend-difference"
+          id="hoverElement"
+        >
+          <h1 className="text-white">Tim</h1>
+          <span>Developer & Student</span>
+          <span>Austria 2024</span>
+        </div>
+      </section>
+      <section className="h-[50vh] w-screen flex justify-center items-center">
+        <p className="sm:w-2/6 w-[80vw]">
+          Hello! I'm Tim, a 17-year-old enthusiast from Austria, deeply
+          engrossed in the world of IT and Web Development. I'm currently
+          immersing myself in coding and design, eager to unlock new
+          possibilities and innovative solutions in this dynamic field.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default LandingPage;
