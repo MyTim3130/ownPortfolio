014bc63913ea521900cbc785022e01e2f6f02677
MyTim3130 <tin.hauslkramper@gmail.com>
2024-02-04 14:34:52 +0100

    minusVideo

--- components/WaterSection.tsx
+++ components/WaterSection.tsx
@@ -9,14 +9,14 @@ const WaterSection = () => {
         <h2 className="absolute z-10 text-6xl lg:text-9xl font-extrabold mix-blend-difference mb-96">
           CONTACT ME
         </h2>
-        <video
+        {/* <video
           autoPlay
           loop
           muted
           className="z-0 absolute h-screen w-screen object-cover rotate-180 saturate-50"
         >
           <source src="/images/waves.mp4" type="video/mp4" />{" "}
-        </video>
+        </video> */}
         <div className="flex flex-col md:flex-row h-[40vh] justify-evenly items-center md:h-fit w-screen absolute bottom-40 font-bold text-xl">
           <span>TIN.HAUSLKRAMPER@GMAIL.COM</span>
           <span>+43 660 133 0906</span>
